080d4b4dc154fbfae4ab23810d1e80a42d4bb02b
AnnaSatolina <annadorohovich@gmail.com>
2025-03-17 22:18:00 +0300

    Change gitignore. Add file for sum

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 int main(){
   std::cout << "hello!\n";
+  std::string str("qqqq");
+  std::cout << str << "\n";
+
+  std::string str1(std::move(str));
+  std::cout << str <<std::endl << str1;
   return 0;
 }
   
