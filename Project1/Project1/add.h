529c93a3e9521d669e22e4cadc29aee827137808
AnnaSatolina <annadorohovich@gmail.com>
2025-03-17 22:50:35 +0300

    Add is correct

--- Project1/Project1/add.h
+++ Project1/Project1/add.h
@@ -1,1 +1,2 @@
 #pragma once
+int add(int, int);
--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 int main(){
   std::cout << "hello!\n";
+  std::string str("qqqq");
+  std::cout << str << "\n";
+
+  std::string str1(std::move(str));
+  std::cout << str <<std::endl << str1;
+  int x = 5;
+  int&& y = std::move(x);
+
+  int&& z = 6;
+  y = 7;
+
+  int&& t = static_cast<int&&>(x);
   return 0;
 }
   
